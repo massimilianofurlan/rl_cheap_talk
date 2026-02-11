c4597431f05936e5a5efaaf5b2b94282663f6ff3
Massimiliano Furlan <frl.msm95@gmail.com>
2026-02-11 17:48:48 +0000

    critical! fixed bug
    temperature schedule did not match simulations' temperature schedule
    
    the bug was introduced in May 2024: forgot to update those two lines

--- scripts/read_data.jl
+++ scripts/read_data.jl
@@ -83,8 +83,8 @@ function extract_data(config, results, extracted_data)
 	Threads.@threads for z in 1:n_converged
 		is_converged[z] == true || continue
         # get policies at convergence
-       	policy_s_ = get_policy(Q_s[:,:,z], max(temp0_s*lambda_s^(n_episodes[z]-1),1f-30))
-        policy_r_ = get_policy(Q_r[:,:,z], max(temp0_r*lambda_r^(n_episodes[z]-1),1f-30))
+		policy_s_ = get_policy(Q_s[:,:,z], max(temp0_s * exp(-lambda_s*(n_episodes[z]-1)), 1f-30))
+		policy_r_ = get_policy(Q_r[:,:,z], max(temp0_r * exp(-lambda_r*(n_episodes[z]-1)), 1f-30))
         policy_s[:,:,z], policy_r[:,:,z] = order_policies(policy_s_, policy_r_)
         # compute induced actions at convergence
         induced_actions[:,:,z] = get_induced_actions(policy_s_, policy_r_)    
